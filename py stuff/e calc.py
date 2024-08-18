86b30cd4a1b90cb41187cbff41029602c11c3954
moiSentineL <nibir.patowary@gmail.com>
2024-08-18 14:50:43 +0530

    some updates

--- py stuff/e calc.py
+++ py stuff/e calc.py
@@ -1,12 +1,7 @@
 from sympy import * # This package allows you to do complex mathematical equations
 
+x = symbols("x")
 iteration = 0 # where x = iterations
 while True:
     iteration += 1000
-
-    x = symbols("x")
-    expression = (1+1/x)**x # (1+1/x)^x
-
-    form = limit(expression, x, iteration)
-
-    print(f"When x = {iteration} -> {float(form)}")
+    print(f"When x = {iteration} -> {float(limit((1+1/x)**x, x, iteration))}")
--- py stuff/ne.py
+++ py stuff/ne.py
@@ -1,2 +1,3 @@
 import random
-for _ in random.sample([file for file in open("py stuff/needs_work.txt", "r")], 20): print(_)
+with open("new", "w") as f: 
+    for _ in random.sample([file for file in open("py stuff/needs_work.txt", "r")], 20): f.write(_) 
